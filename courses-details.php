72bedbb376ea1e83eea8cc89b0f16692ab7ce17d
BlackStoneGameDevelopment <60821305+BlackStoneGameDevelopment@users.noreply.github.com>
2023-01-13 14:56:12 +0530

    Updated

--- courses-details.php
+++ courses-details.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Courses Details || Edubuzz</title>
+        <title>Courses Details || FIAAT</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
         
